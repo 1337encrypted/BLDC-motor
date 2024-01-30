6a768cc1a935432080b39cc07b389731a92eda18
1337encrypted <smallaszero@gmail.com>
2024-01-30 18:38:54 +0530

    converted the BLDC_speed code to idf

---
+++ BLDC_speed/main/BLDC_speed.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "BLDCPulseCalculator.h"
+
+// Use only core 1 for demo purposes
+#if CONFIG_FREERTOS_UNICORE
+  static const BaseType_t app_cpu = 0;
+#else
+  static const BaseType_t app_cpu = 1;
+#endif
+
+
+// Motor1 
+constexpr gpio_num_t wavePin1 = GPIO_NUM_2;
+constexpr uint8_t motorId1 = 1;
+
+/* Object creation */
+BLDCPulseCalculator PMSMMotor1(wavePin1, motorId1);
+
+
+extern "C"
+{
+  void app_main(void)
+  {
+    PMSMMotor1.begin(app_cpu);
+  }
+}
