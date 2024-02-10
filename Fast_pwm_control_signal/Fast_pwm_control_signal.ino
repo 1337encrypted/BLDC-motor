1598cfd00941aae4e8b9c32c60d5c740a73d9499
1337encrypted <smallaszero@gmail.com>
2024-02-10 19:46:01 +0530

    modified fast pwm control

--- Fast_pwm_control_signal/Fast_pwm_control_signal.ino
+++ Fast_pwm_control_signal/Fast_pwm_control_signal.ino
@@ -2,12 +2,12 @@
 
 constexpr uint8_t pwmPin = 9; // Choose a PWM-capable pin (e.g., D9)
 // Settings
-static const uint8_t buf_len = 20;
+// static const uint8_t buf_len = 20;
 
-char c;         
-char buf[buf_len];
-uint8_t idx = 0;
-int dutyCycle = 0;
+// char c;         
+// char buf[buf_len];
+// uint8_t idx = 0;
+// int dutyCycle = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -15,43 +15,43 @@ void setup() {
   pinMode(pwmPin, OUTPUT);
   
   // Set up Timer1 with a frequency of 100 kHz
-  Timer1.initialize(500); // Timer period in microseconds (1/100,000 Hz = 10 microseconds)
-  Timer1.pwm(pwmPin, 0);
+  Timer1.initialize(10); // Timer period in microseconds (1/100,000 Hz = 10 microseconds)
+  Timer1.pwm(pwmPin, 512);
 }
 
-void clearBuffer()
-{
-  for (int i = 0; i < buf_len; i++) 
-  {
-    buf[i] = 0;
-  }
-}
+// void clearBuffer()
+// {
+//   for (int i = 0; i < buf_len; i++) 
+//   {
+//     buf[i] = 0;
+//   }
+// }
 
 void loop() 
 {
-  // Read characters from serial
-  while (Serial.available() > 0) 
-  {
-    c = Serial.read();
+  // // Read characters from serial
+  // while (Serial.available() > 0) 
+  // {
+  //   c = Serial.read();
 
-    // Update dutyCycle variable if we get a newline character
-    if (c == '\n') {
+  //   // Update dutyCycle variable if we get a newline character
+  //   if (c == '\n') {
 
-      dutyCycle = atoi(buf);
+  //     dutyCycle = atoi(buf);
 
-      Serial.println(dutyCycle);
-      Timer1.pwm(pwmPin, dutyCycle); // PWM duty cycle (0 to 1023, where 0 is 0% and 1023 is 100%)
-      clearBuffer();
-      idx = 0;
-    } 
-    else
-    {
-      // Only append if index is not over message limit
-      if (idx < buf_len - 1) 
-      {
-        buf[idx] = c;
-        idx++;
-      }
-    }
-  }
+  //     Serial.println(dutyCycle);
+  //     Timer1.pwm(pwmPin, dutyCycle); // PWM duty cycle (0 to 1023, where 0 is 0% and 1023 is 100%)
+  //     clearBuffer();
+  //     idx = 0;
+  //   } 
+  //   else
+  //   {
+  //     // Only append if index is not over message limit
+  //     if (idx < buf_len - 1) 
+  //     {
+  //       buf[idx] = c;
+  //       idx++;
+  //     }
+  //   }
+  // }
 }
